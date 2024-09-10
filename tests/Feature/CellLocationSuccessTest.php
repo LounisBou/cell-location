9d57a61b4cc355fef46b3e421e9111f72aec9edd
LounisBou <lounis.bou@gmail.com>
2024-09-10 14:49:26 +0200

    Correct CellLocationSuccessTest

--- tests/Feature/CellLocationSuccessTest.php
+++ tests/Feature/CellLocationSuccessTest.php
@@ -1,9 +1,10 @@
 <?php
 
 use Lounisbou\CellLocation\CellLocator;
-use Lounisbou\CellLocation\OpenCellIDService;
-use Lounisbou\CellLocation\UnwiredLabsService;
-use Lounisbou\CellLocation\GoogleGeolocationService;
+use Lounisbou\CellLocation\RadioType;
+use Lounisbou\CellLocation\Services\UnwiredLabsService;
+use Lounisbou\CellLocation\Services\OpenCellIDService;
+use Lounisbou\CellLocation\Services\GoogleGeolocationService;
 
 test('findLocation works with OpenCellID service', function () {
     // Create an instance of the OpenCellID service
@@ -13,11 +14,17 @@ test('findLocation works with OpenCellID service', function () {
     $cellLocator = new CellLocator($openCellIdService);
 
     // Expect the output to match the known latitude and longitude
-    $this->expectOutputString('Latitude: 37.7749, Longitude: -122.4194');
+    $this->expectOutputString('Latitude: 52.231644, Longitude: 21.011933');
     
     // Test the findLocation method with known cell location data
-    $cellLocation = $cellLocator->getLocation(310, 410, 0, 0);
-
+    $cellLocation = $cellLocator->getLocation(
+        mcc:260, 
+        mnc:2,
+        lac:10250,
+        cellId:26511,
+        radioType:RadioType::GSM
+    );
+    
     // Print the cell location
     echo $cellLocation;
 });
@@ -30,10 +37,16 @@ test('findLocation works with UnwiredLabs service', function () {
     $cellLocator = new CellLocator($unwiredLabsService);
 
     // Expect the output to match the known latitude and longitude
-    $this->expectOutputString('Latitude: 37.7749, Longitude: -122.4194' . PHP_EOL);
+    $this->expectOutputString('Latitude: 52.230743, Longitude: 21.009712');
     
     // Test the findLocation method with known cell location data
-    $cellLocation = $cellLocator->getLocation(310, 410, 0, 0);
+    $cellLocation = $cellLocator->getLocation(
+        mcc:260, 
+        mnc:2,
+        lac:10250,
+        cellId:26511,
+        radioType:RadioType::GSM
+    );
 
     // Print the cell location
     echo $cellLocation;
@@ -47,10 +60,16 @@ test('findLocation works with Google Geolocation service', function () {
     $cellLocator = new CellLocator($googleMapsService);
 
     // Expect the output to match the known latitude and longitude
-    $this->expectOutputString('Latitude: 37.7749, Longitude: -122.4194' . PHP_EOL);
+    $this->expectOutputString('Latitude: 52.231644, Longitude: 21.011933');
     
     // Test the findLocation method with known cell location data
-    $cellLocation = $cellLocator->getLocation(310, 410, 0, 0);
+    $cellLocation = $cellLocator->getLocation(
+        mcc:260, 
+        mnc:2,
+        lac:10250,
+        cellId:26511,
+        radioType:RadioType::GSM
+    );
 
     // Print the cell location
     echo $cellLocation;
